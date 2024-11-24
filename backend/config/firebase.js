99d454b7c88b973a7bafc47e383968084f8e00ae
Martina Di Paola <giovyp73@gmail.com>
2024-11-24 19:43:09 +0900

    firebase.js

---
+++ backend/config/firebase.js
@@ -0,0 +1,12 @@
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://databasequiz-8aaf0-default-rtdb.firebaseio.com/"
+});
+
+const db = admin.firestore();
+
+module.exports = db;
