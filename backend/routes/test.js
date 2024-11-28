9ea22ae54c6a58d771abfbdba83d77624e3659ac
shahnozayadgar <yadgarova@kaist.ac.kr>
2024-11-28 15:02:41 +0900

    extracting one questions at a time

--- backend/routes/test.js
+++ backend/routes/test.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const {questions} = require('../data');
 
 //end point to get all scenarios and questions
-router.get('/questions', (req, res) => {
-    res.json(questions);
+router.get('/questions/:id', (req, res) => {
+    const questionID = req.params.id;
+    const question = questions.find((question)=> question.id === parseInt(questionID));
+    if (!question) {
+        return res.status(404).json({message: 'Question not found'});
+    }
+    res.json(question);
 });
 module.exports = router;
