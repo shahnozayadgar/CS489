a80014fa1510b6d01c68dbe14527705c32dad6e2
marticodes <125369224+marticodes@users.noreply.github.com>
2024-12-03 19:47:33 +0900

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,45 +1,35 @@
 class User {
-  //adding dropdown options as valid options 
-  //will need to change this later!!!
-  static validDegreeTypes = ["Bachelor", "Master", "PhD"];
-  static validMajors = ["Computer Science"];
-  static validGenders = ["Male", "Female", "Non-binary", "Prefer not to say"];
-  static validMbti = ["I", "E", "N", "S", "T", "F", "J", "P"];
+  constructor(userId, name, degreeType, major, gender, mbti = null) {
+    this.userId = userId; // Connect to questions
+    this.name = name;
+    this.degreeType = degreeType; // Like Bachelor, etc.
+    this.major = major; // Their department
+    this.gender = gender;
+    this.mbti = mbti;
+  }
 
-    constructor(userId, name, degreeType, major, gender, mbti = null) {
-        this.userId = userId; //connect to questions
-        this.name = name;
-        this.degreeType = degreeType; // like bachelor etc
-        this.major = major; // their department
-        this.gender = gender; 
-        this.mbti = mbti; 
-    }
-  
-    // check if all required fields are present
-    isValid() {
-      return (
-        this.userId &&
-        this.name &&
-        this.degreeType &&
-        this.major &&
-        this.gender &&
-        typeof this.name === "string" &&
-        typeof this.degreeType === "string" &&
-        typeof this.major === "string" &&
-        typeof this.gender === "string" &&
-        this.validDegreeTypes.includes(this.degreeType) &&
-        this.validMajors.includes(this.major) &&
-        this.validGenders.includes(this.gender) &&
-        this.validMbti.includes(this.mbti)
-      );
+  // Check if all required fields are present (non-empty)
+  isValid() {
+    return (
+      this.userId &&
+      this.name &&
+      this.degreeType &&
+      this.major &&
+      this.gender &&
+      typeof this.name === "string" &&
+      typeof this.degreeType === "string" &&
+      typeof this.major === "string" &&
+      typeof this.gender === "string"
+    );
   }
 
   static generateUserId(existingUserIds) {
-      // Get the maximum existing user ID and increment by 1 for the new user
-      const maxId = existingUserIds.length ? Math.max(...existingUserIds) : 0;
-      return maxId + 1;
+    // Get the maximum existing user ID and increment by 1 for the new user
+    const maxId = existingUserIds.length ? Math.max(...existingUserIds) : 0;
+    return maxId + 1;
   }
 }
 
 module.exports = User;
+
 
